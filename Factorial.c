4740b58b88b0f5c32c98831e605959a48c19d265
vijapure11 <118713664+vijapure11@users.noreply.github.com>
2022-11-21 12:37:11 +0530

    create factrial.c

---
+++ Factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+  int n,i,fact=1;
+  printf("\n Enter a number: ");
+  scanf("%d",&n);
+}
+  while(n>0)
+  if(i=1;i<n;i++)
+  {
+    fact=fact*i;
+  }
+printf("\n Factorial=%d",fact);
+  
+   
